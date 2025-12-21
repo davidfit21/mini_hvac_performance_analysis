cee55d58531b154b2dc58c5ef364753d9608d90c
David Fit <david.fit2@mail.dcu.ie>
2025-12-21 15:07:41 +0000

    other

--- launch.py
+++ launch.py
@@ -2,14 +2,7 @@ import streamlit as st
 from apps import app_mini_hvac, app_plant_hvac  # assume you move your app code into an `apps` folder
 
 # Must be first Streamlit command
-st.set_page_config(page_title="HVAC Launcher", layout="wide")#, page_icon="🏭")
-
-# UI header
-col1, col2 = st.columns([5, 1])
-with col1:
-    st.title("HVAC Analysis Launcher")
-with col2:
-    st.image("htms_logo.jpg", width=150)
+st.set_page_config(page_title="HVAC Analysis Launcher", layout="wide")
 
 # Page selection
 option = st.selectbox("Choose Analysis:", ["Mini-HVAC Analysis", "Plant-HVAC Analysis"])
