1b29679f42453ea6a8d57ab99047c727a4612214
David Fit <david.fit2@mail.dcu.ie>
2025-12-22 00:36:38 +0000

    updated both apps

--- launch.py
+++ launch.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Plant-HVAC",
+    page_title="HVAC-Analysis",
     layout="wide",
     page_icon="htms_logo.jpg")
+
+    # Page header
+col1, col2 = st.columns([5,1])
+#with col1:
+with col2:
+        st.image("htms_logo.jpg", width=150)
 
 from apps import app_mini_hvac, app_plant_hvac
 
